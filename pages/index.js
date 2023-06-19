10d8c0ca7a51d8ceeaa22744da584d7b2210ca15
moinhaider520 <moin.haider.520@gmail.com>
2023-06-19 01:18:35 -0700

    NEW

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+export default function Home() {
+  const [socket_state, setSocket_state] = useState('try connecting...');
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    fetch('api/socket');
+    setSocket(io());
+  }, []);
+  if (socket) {
+    socket.on('connect', () => {
+      console.log('connected successfully', socket.id);
+      setSocket_state('connected successfully 👍');
+    });
+  }
+  return (
+    <>
+      <div>
+        <h1>socket state: {socket_state}</h1>
+      </div>
+    </>
+  );
+}
